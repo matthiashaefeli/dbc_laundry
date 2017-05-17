e88291ebfedd72a2f7e9df7a0dc45d7191a44870
RCAVelez <chve38@gmail.com>
2017-05-17 16:04:49 -0500

    Basic validations for order in model

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,7 @@ class Order < ApplicationRecord
 	belongs_to :client
 	belongs_to :business
 	has_many :items
+	validates :client_id, presence: true
+	validates :business_id, presence: true
+	validates :box_in, presnece: true
 end
