4b5928e437a6438047e6aa815dd52202ec6178b9
RCAVelez <chve38@gmail.com>
2017-05-17 16:16:11 -0500

    Controller: Order, + index method

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,5 +3,11 @@ class OrdersController < ApplicationController
 		@order = Order.new #Need to pass params of order details
 	end
 
-	
+	def show
+		@order = Order.find(params[:id]) #Show an order
+	end
+
+	def index 
+		@orders = Order.where(client_id = ?, current_user.id) #Show all user orders, needs devise to work
+	end
 end
