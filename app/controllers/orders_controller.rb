69f2808445271770ae6316f0fb46db216ba7a0d2
RCAVelez <chve38@gmail.com>
2017-05-18 10:02:04 -0500

    Created JS file for qrcode action, NewOrder: changed add link to a button

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,7 @@ class OrdersController < ApplicationController
 
 	def create
 		@order = Order.create(client_id: current_client.id, business_id: 1, box_in: 5, status: "In Box", paid: false)
+		
 		render "./home.html.erb"
 	end
 
