84fd5424dd5a40d69ccee1dc01a44fae2b1ffa6f
RCAVelez <chve38@gmail.com>
2017-05-18 12:47:12 -0500

    Added form around button

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,11 +10,11 @@ class OrdersController < ApplicationController
 	def create
 		@order = Order.create(client_id: current_client.id, business_id: 1, box_in: 5, status: "In Box", paid: false)
 		
-		render "./home.html.erb"
+		
 	end
 
 	def update
-		binding.pry
+		
 		@order = Order.find(params[:id])
 		@order.update_attributes() #missing params from select to update @order
 	end
