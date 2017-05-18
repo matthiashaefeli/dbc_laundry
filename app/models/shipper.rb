730e790190dae74567e97886e67ae494a7bb58a7
matthiashaefeli <haefeli.matthias@gmail.com>
2017-05-18 13:49:39 -0500

    add business show page. add column name and business_id to shipper, add column business_id to admin

--- app/models/shipper.rb
+++ app/models/shipper.rb
@@ -1,4 +1,5 @@
 class Shipper < ApplicationRecord
+	belongs_to :business
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,  :registerable,
