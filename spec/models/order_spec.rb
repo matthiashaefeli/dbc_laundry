965bde1f5eff455b3f655fae298e66ad77608490
Karina Franco <email": null>
2017-05-19 14:30:14 -0500

    order model spec

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,19 +2,25 @@ require 'rails_helper'
 
 describe Order, type: :model do
   let(:business){Business.new(name: "Wishi washy", email: "wishy@washy.com", hash_password: "password")}
-  let(:client){Client.new(name: "Client", phone: "123-111-2354",email: "client@client.com", encrypted_password: "password", business_id: business.id)}
-  let(:order_shipped){Order.new(box_in: 1,client_id: client.id, business_id: business.id, status: "Shipped", total: 30, paid: false)}
+
+  let(:client){Client.new(name: "Client", phone: "123-111-2354",email: "client@client.com", encrypted_password: "password")}
+
+  let(:order_shipped){Order.new(box_in: 1, status: "Shipped", total: 30, paid: false)}
 
   it "has a status" do
     expect(order_shipped.status).to eq "Shipped"
   end
 
-  it "belongs to a client" do
-    expect(order_shipped.client).to eq client
-  end
+  describe "belongs to" do
+    it "belongs to a client" do
+       order_shipped.client = client
+      expect(order_shipped.client).to eq client
+    end
 
-  it "belongs to a business" do
-    expect(order_shipped.business).to eq business
+    it "belongs to a business" do
+      order_shipped.business = business
+      expect(order_shipped.business).to eq business
+    end
   end
 
   describe "can be paid" do
@@ -31,7 +37,4 @@ describe Order, type: :model do
       expect(order_shipped.paid).to eq true
     end
   end
-
-
-
 end
