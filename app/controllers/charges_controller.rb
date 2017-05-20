a545474359505dc24ffa6a004c41873d465bfed9
Karina Franco <email": null>
2017-05-20 12:17:03 -0500

    add routes to charges

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -16,15 +16,19 @@ class ChargesController < ApplicationController
       :customer => customer.id,
       :amount => @amount,
       :description => "Bag of clothes",
+      :statement_descriptor => "Dbc Laundry"
       :currency => 'usd'
       )
     @order = current_client.orders.last
     @order.paid = true
+    @order.total = @amount/100
+    @order.save
+
   rescue Stripe::CardError => e
     @order.paid = false
     flash[:error] = e.message
     redirect_to new_order_path
   end
 
-  redirect_to root_path
+
 end
