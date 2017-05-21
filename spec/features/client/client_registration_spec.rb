db1c4b1e3bc51acf94984629a636b9adc46b85c1
karinafraest <karinafraest@gmail.com>
2017-05-21 12:54:51 -0500

    invalid client registration spec

--- spec/features/client/client_registration_spec.rb
+++ spec/features/client/client_registration_spec.rb
@@ -21,4 +21,20 @@ feature "Client register" do
   end
 
 
+scenario "Invalid client can register" do
+    business = Business.create(name: "wash", email: "wash@wash.com", hash_password: "password")
+    visit new_client_registration_path
+    within(".new_client") do
+      fill_in("Name", with: "Client")
+      fill_in("Phone", with: "5124242424")
+      select "wash", :from => "client[business_id]"
+      fill_in("Password", with: "password")
+      fill_in("Password Confirmation", with: "password")
+    end
+
+    click_on("Sign up")
+
+    expect(page).to have_content "Email can't be blank"
+  end
+
 end
