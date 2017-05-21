ce9931489a5b9cf743cfcb52a5eba41fd06a2be6
karinafraest <karinafraest@gmail.com>
2017-05-21 12:52:44 -0500

    valid registration spec

---
+++ spec/features/client/client_registration_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+feature "Client register" do
+  scenario "Valid client can register" do
+    business = Business.create(name: "wash", email: "wash@wash.com", hash_password: "password")
+    visit new_client_registration_path
+    within(".new_client") do
+      fill_in("Name", with: "Client")
+      fill_in("Phone", with: "5124242424")
+      fill_in("Email", with: "new.client@nclient.com")
+      select "wash", :from => "client[business_id]"
+      fill_in("Password", with: "password")
+      fill_in("Password Confirmation", with: "password")
+    end
+
+    click_on("Sign up")
+
+    expect(page).to have_content "Welcome! You have signed up successfully.
+
+"
+  end
+
+
+end
