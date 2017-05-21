30f7cccd2718b8d2e22ad1693409428ca5ff7974
karinafraest <karinafraest@gmail.com>
2017-05-21 11:13:17 -0500

    can send emails in dev

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -19,7 +19,23 @@ class UserNotifier < ApplicationMailer
     sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
 
     response = sg.client.mail._('send').post(request_body: mail.to_json)
+    puts response.status_code
+    puts response.body
+    puts response.headers
+  end
 
+
+  def hello_world
+    from = Email.new(email: 'test@example.com')
+    subject = 'Hello World from the SendGrid Ruby Library'
+    to = Email.new(email: 'test@example.com')
+    content = Content.new(type: 'text/plain', value: 'some text here')
+    mail = Mail.new(from, subject, to, content)
+    # puts JSON.pretty_generate(mail.to_json)
+    puts mail.to_json
+
+    sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'], host: 'https://api.sendgrid.com')
+    response = sg.client.mail._('send').post(request_body: mail.to_json)
     puts response.status_code
     puts response.body
     puts response.headers
