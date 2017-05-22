a9b5a28592c18c7cbd6dd50528a3622feee1615b
karinafraest <karinafraest@gmail.com>
2017-05-21 20:22:45 -0500

    client can create new order

--- spec/features/client/client_makes_new_order_spec.rb
+++ spec/features/client/client_makes_new_order_spec.rb
@@ -19,4 +19,31 @@ feature "Client can make a new order" do
     expect(page).to have_content "Set Box Direction"
   end
 
+  scenario "Client creates a new order" do
+    business = Business.create(name: "wash", email: "wash@wash.com", hash_password: "password")
+    client = Client.create(name: "Client", phone: "1231234123", business: business, email: "client@client.com", password: "password")
+    box = Box.create(name: 'University', business: business, address: 'Guadalupe St')
+    visit new_client_session_path
+    within(".new_client") do
+      fill_in("Enter Email", with: 'client@client.com')
+      fill_in("Password", with: 'password')
+    end
+    click_on('Log in')
+
+    within(".admin-header") do
+      click_on("New Order")
+    end
+
+    visit new_order_path
+
+    within(".container") do
+      select "Guadalupe St", :from => "orders[pick_up_address]"
+    end
+    click_on('Save Orders')
+
+    expect(page).to have_current_path new_charge_path
+    expect(page).to have_content "Amount:"
+
+
+  end
 end
