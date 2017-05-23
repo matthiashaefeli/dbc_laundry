9832fe296383b9b8e89f7eaf201b30ab68f27f0b
Karina Franco <email": null>
2017-05-23 17:42:42 -0500

    remove pry

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -14,7 +14,6 @@ class UserNotifier < ApplicationMailer
     sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
 
     response = sg.client.mail._('send').post(request_body: mail.to_json)
-    binding.pry
     puts response.status_code
     puts response.body
     puts response.headers
