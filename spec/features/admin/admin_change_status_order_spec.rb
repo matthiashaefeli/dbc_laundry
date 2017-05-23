9df09d37e4fec1199aefdf5f3519989e307d9adb
Karina Franco <email": null>
2017-05-23 10:14:38 -0500

    fix admin spec

--- spec/features/admin/admin_change_status_order_spec.rb
+++ spec/features/admin/admin_change_status_order_spec.rb
@@ -2,16 +2,16 @@ require 'rails_helper'
 
 
 
- feature 'Admin clicks on a drop down menu and changes order status' do 
+ feature 'Admin clicks on a drop down menu and changes order status' do
  	 scenario "Valid admin can log in" do
-  	#ADMIN LOGS IN 
+  	#ADMIN LOGS IN
   	business = Business.create(name: "wash", email: "wash@wash.com", hash_password: "password")
   	admin = Admin.create(name: "Admin", business: business, email: "admin@admin.com", password: "password")
   	box = Box.create(name: 'Frost Bank', business_id: business.id, address: 'Barbara Jordan Blvd')
-  	
+
   	client = Client.create(name: "Mr Happy face", password: "password", email: "happy@client.com", phone: "4993333333", business_id: business.id, bag_id: 1234)
   	order = Order.create(id: 1, box_in: 1, box_out:1, client: client, business: business, status: 'Incomming', total: 100, paid: true)
-  	
+
   	visit new_admin_session_path
   	within(".container") do
   		fill_in("Email", with: 'admin@admin.com')
@@ -19,8 +19,10 @@ require 'rails_helper'
   	end
   	click_on('Log in')
   	expect(page).to have_current_path root_path
-  	
 
-  	select('Processing', :from => 'order[order_status]')
+    within(".remove-and-insert") do
+  	 select('Processing', :from => 'order[order_status]')
+    end
+    click_on("update")
   end
 end
