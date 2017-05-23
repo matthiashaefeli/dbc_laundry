d88392892e2d6b9bf3cd6eb2066901e00316039c
Karina Franco <email": null>
2017-05-23 10:59:58 -0500

    admin can edit client bags

--- spec/features/admin/admin_can_manage_bags_spec.rb
+++ spec/features/admin/admin_can_manage_bags_spec.rb
@@ -42,4 +42,29 @@ feature 'Admin can manage bags' do
 
     expect(page).to have_content "Bag is already in use"
   end
+
+  scenario "Admin cannot assign a bag that has already been assigned" do
+    business = Business.create(name: "wash", email: "wash@wash.com", hash_password: "password")
+    admin = Admin.create(name: "Tim", business: business, email: "admin@admin.com", password: "password")
+    box = Box.create(name: 'Frost Bank', business_id: business.id, address: 'Barbara Jordan Blvd')
+    client1 = Client.create(name: "Mr Cheery Face", password: "password", email: "chery@client.com", phone: "9333333334", business_id: business.id, bag_id: "12345")
+    client = Client.create(name: "Mr Happy face", password: "password", email: "happy@client.com", phone: "4993333333", business_id: business.id)
+    visit new_admin_session_path
+    within(".container") do
+      fill_in("Email", with: 'admin@admin.com')
+      fill_in("Password", with: 'password')
+    end
+    click_on('Log in')
+    visit business_path(business)
+
+    click_on("12345")
+    expect(page).to have_current_path edit_bag_path(client1)
+
+    within(".edit_client") do
+      fill_in("client[bag_id]", with: "55555")
+    end
+    click_on("Update Client")
+
+    expect(page).to have_content "55555"
+  end
 end
